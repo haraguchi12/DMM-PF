940a334c3e2678b14f34d78d45ae232a32eca528
haraguchi12 <masakazu09201201@gmail.com>
2021-06-17 09:44:22 +0000

    [追加]投稿機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -45,4 +45,10 @@ class PostsController < ApplicationController
     @post.destroy
     redirect_to posts_path
   end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:image, :caption)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,4 @@
 class Post < ApplicationRecord
   belongs_to :store
+  attachment :image
 end
