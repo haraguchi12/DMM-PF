cd9a2f5af98ed48a9047cd1572705bde65d5f3fe
haraguchi12 <masakazu09201201@gmail.com>
2021-06-21 08:20:54 +0000

    [修正]devise設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,21 @@
 Rails.application.routes.draw do
   #deviseの設定
    devise_for :users, skip: :all
-     devise_scope :users do
+     devise_scope :user do
       get '/users/sign_in' => 'user/sessions#new', as: :new_user_session
       post '/users/sign_in' => 'user/sessions#create', as: :user_session
       delete '/users/sign_out' => 'user/sessions#destroy', as: :destroy_user_session
+      get '/users/sign_up' => 'user/registrations#new', as: :new_user_registration
+      post '/users'=> 'user/registrations#create', as: :user_registration
    end
-   
+
    devise_for :stores, skip: :all
     devise_scope :store do
     get '/stores/sign_in' => 'store/sessions#new', as: :new_store_session
     post '/stores/sign_in' => 'store/sessions#create', as: :store_session
     delete '/stores/sign_out' => 'store/sessions#destroy', as: :destroy_store_session
     get '/stores/sign_up' => 'store/registrations#new', as: :new_store_registration
-    post '/stores'=> 'store/registrations#create'
+    post '/stores'=> 'store/registrations#create', as: :store_registration
    end
 
     #ホーム
