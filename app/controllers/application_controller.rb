e5c4b2fe2a81ba4408fbe80226a622202f979202
haraguchi12 <masakazu09201201@gmail.com>
2021-06-22 13:44:32 +0000

    [追加]モデル別でログイン後を分岐設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  def after_sign_out_path_for(resource)
+   root_path
+  end
+
+  def after_sign_in_path_for(resource)
+    case resource
+    when User
+      user_path
+
+    when Store
+       store_path
+    end
+  end
 end
