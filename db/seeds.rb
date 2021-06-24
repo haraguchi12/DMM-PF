ea26ef82db83296d234bd564eb9f691a6e1c1801
haraguchi12 <masakazu09201201@gmail.com>
2021-06-24 10:32:50 +0000

    [追加]seedsにユーザーと店舗を追加

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Store.create(name: '麻屋商店', address: '埼玉県秩父市本町3-6', postal_code: '3680044', telephone_number: '11122223333', email: 'asaya@chichibu.com', password: '111111')
+
+User.create(email: 'user1@chichibu.com', password: '111111')
