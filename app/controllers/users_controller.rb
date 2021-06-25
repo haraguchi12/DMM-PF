68394e1dcd28fde9c615a27533fc7d618aea9424
haraguchi12 <masakazu09201201@gmail.com>
2021-06-25 14:08:09 +0000

    [追加]お気に入り機能　アクション

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,11 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
+
+
+    favorites = Favorite.where(user_id: current_user.id).pluck(:store_id)  # ログイン中のユーザーのお気に入りのstore_idカラムを取得
+    @favorite_list = Store.find(favorites)     # storesテーブルから、お気に入り登録済みのレコードを取得
   end
 
   def edit
